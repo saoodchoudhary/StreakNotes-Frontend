8c883402ea6f962430c52ef63a43f19ca0efebff
Saood Ahmad <99206626+saoodchoudhary@users.noreply.github.com>
2024-07-11 23:25:41 +0530

    change color favi add ;

--- src/components/homepage/AddFavicon.jsx
+++ src/components/homepage/AddFavicon.jsx
@@ -3,7 +3,7 @@ import { IoMdAdd } from "react-icons/io";
 import { Link } from 'react-router-dom';
 const AddFavicon = () => {
   return (
-    <Link to={"/addNotes/someid"} className='fixed bottom-[65px] inline-block right-[30px] bg-green-600 z-30 p-[10px] rounded-[50%]'>
+    <Link to={"/addNotes/someid"} className='fixed bottom-[65px] inline-block right-[30px] bg-gradient-to-t from-teal-400 to-blue-400 z-30 p-[15px] rounded-[50%]'>
       <IoMdAdd className='h-[20px] w-auto text-white self-center'/>
     </Link>
   )
