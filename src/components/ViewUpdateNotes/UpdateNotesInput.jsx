d4eab91a43fbf32533a3215f1b54ce84292d2baa
Saood Ahmad <99206626+saoodchoudhary@users.noreply.github.com>
2024-07-13 16:02:01 +0530

    optimize: update page redux optimize

--- src/components/ViewUpdateNotes/UpdateNotesInput.jsx
+++ src/components/ViewUpdateNotes/UpdateNotesInput.jsx
@@ -1,21 +1,28 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { AiOutlineBold, AiOutlineItalic, AiOutlineOrderedList, AiOutlineUnorderedList } from 'react-icons/ai';
 import { BiHeading } from 'react-icons/bi';
-import { MdFormatColorText } from 'react-icons/md';
+import { MdFormatColorText, MdKeyboardArrowRight } from 'react-icons/md';
 import axios from 'axios';
 import { FaImage } from 'react-icons/fa';
 import { IoSend } from 'react-icons/io5';
 import SendNotesComp from './UpdateSendNotesComp';
+import { useDispatch, useSelector } from 'react-redux';
+import { postSaveNote, setIsSaved } from '../../redux/slice/noteSlice';
 
 const UpdateNotesInput = ({nId}) => {
   const editorRef = useRef(null);
-  console.log(nId);
-  const [noteId, setNoteId] = useState(nId); // to store the note ID
+  const allBtnRef = useRef(null);
+  const noteId = nId; // to store the note ID
   const [content, setContent] = useState('');
   const [isSendNotes, setIsSendNotes] = useState(false); 
-  const [isSaved, setIsSaved] = useState(false); // to check if the note is saved or not
+  // const [isSaved, setIsSaved] = useState(false); // to check if the note is saved or not
   const [isLoading, setIsLoading] = useState(false); // to check if the note is saved or not
   const [firstTimeLoad, setFirstTimeLoad] = useState(true);
+  const dispatch = useDispatch();
+
+  // bottom height of the editor
+  const [bottomHeight, setBottomHeight] = useState(0);
+
   const [activeFormats, setActiveFormats] = useState({
     bold: false,
     italic: false,
@@ -28,12 +35,16 @@ const UpdateNotesInput = ({nId}) => {
     greenText: false,
   });
 
+  const notes = useSelector(state => state.note);
+  const isSaved = notes.isSaved;
+
   const formatText = (command, value = null) => {
     document.execCommand(command, false, value);
     editorRef.current.focus();
     updateActiveFormats();
   };
 
+  
   const updateActiveFormats = () => {
     setActiveFormats({
       bold: document.queryCommandState('bold'),
@@ -49,6 +60,27 @@ const UpdateNotesInput = ({nId}) => {
   };
 
   useEffect(() => {
+    visualViewport.addEventListener('resize', () => {
+      const visualViewportHeight = window.visualViewport.height;
+      const innerHeight = document.documentElement.clientHeight;
+      const bottomH =   innerHeight - visualViewportHeight;
+      setBottomHeight(bottomH);
+    });
+
+  }, [bottomHeight]);
+
+  
+  const handleRightScroll = () => {
+    if(bottomHeight > 0){
+      
+    editorRef.current.focus();
+    }
+    allBtnRef.current.scrollTo({
+      left: allBtnRef.current.scrollLeft + 100,
+      behavior: 'smooth',
+    })
+  };
+  useEffect(() => {
     document.addEventListener('selectionchange', updateActiveFormats);
     return () => {
       document.removeEventListener('selectionchange', updateActiveFormats);
@@ -57,42 +89,16 @@ const UpdateNotesInput = ({nId}) => {
 
 
   const saveNotes = async () => {
-    console.log('saving note');
-    setIsLoading(true);
-
-    
-        // get today's date
-        const todayDate = new Date();
-      const dateId = todayDate.toISOString().slice(0, 10);
-
-     axios.post(import.meta.env.VITE_API_URI+'/notes/save-note', { noteId, dateId: dateId,    content: editorRef.current.innerHTML, uid: localStorage.getItem('uid')})
-     .then((response) => { 
-        console.log('response', response.data);
-
-        setIsSaved(true);
-        setIsLoading(false);
-        if (!noteId) {
-          console.log('noteId', response.data.noteId);
-          setNoteId(response.data.noteId);
-        }
-
-     }).catch((error) => {
-       console.error('Failed to save note', error); 
-       setIsLoading(false);
-       });
+    dispatch(postSaveNote({content: editorRef.current.innerHTML, noteId: noteId}));
    
   };
 
   const fetchNotes = async () => {
-
-    console.log("updating notes");
     setIsLoading(true);
     axios.get( import.meta.env.VITE_API_URI + '/notes/fetchOneNotes/'+noteId)
     .then((response) => {
-      console.log('fetch', response.data);
       setContent(response.data.content);
-      setIsLoading(false);
-      
+      setIsLoading(false);      
       setFirstTimeLoad(false);
       
     if (editorRef.current) {
@@ -100,8 +106,7 @@ const UpdateNotesInput = ({nId}) => {
     }
     }).catch((error) => {
       console.error('Failed to fetch note', error);
-      setIsLoading(false);
-      
+      setIsLoading(false);      
       setFirstTimeLoad(false);
     });
   }
@@ -117,7 +122,7 @@ const UpdateNotesInput = ({nId}) => {
     if (firstTimeLoad) {
       return;
     }
-    setIsSaved(false);
+    dispatch(setIsSaved(false));
     console.log(content);
     const saveNote = setTimeout(() => {
        saveNotes();
@@ -130,97 +135,87 @@ const UpdateNotesInput = ({nId}) => {
 
 
   return (
-    <div className="flex flex-col h-full">
-      <div className='fixed top-[15px] right-3 z-50'>
-       {isLoading ? <div className=' h-[22px] w-[22px] mt-1 mr-2 border-[2px] border-t-transparent  animate-spin rounded-full border-blue-500'> </div> : <button className='relative text-blue-500  '>
-          
-          {(isSaved) ? <div className='px-2 py-1 '>Saved</div> : <div  className='px-2 py-1  animate-pulse'>Save.. </div> }
-          </button>  }
-      </div>
-      <div 
-        ref={editorRef} 
-        className="w-full min-h-[90vh] mt-[108px] p-4 bg-white outline-none overflow-auto"
-        contentEditable={true}
+    <div className="flex flex-col w-full" >
+    <div className='fixed top-[0] right-6 z-50'>
+      
+    <div onClick={()=>{setIsSendNotes(prev => !prev)}} className=' flex h-[60px] items-center justify-center align-middle  text-green-700'><IoSend className='self-center'/></div>
+    </div> 
         
-        placeholder="Write your note here..."
-        onInput={() => setContent(editorRef.current.innerHTML)}
-      ></div>
+        {(isSaved) ? <div className='h-2 w-2 bg-green-500 rounded-full fixed top-[70px] right-4 z-40'></div> : <div  className='h-2 w-2 bg-red-500 z-40 rounded-full fixed top-[70px] right-4 animate-pulse'></div> }
+       
+    <div 
+      ref={editorRef} 
+      className="w-full fixed top-[60px]  p-4 bg-white outline-none overflow-y-auto"
+      contentEditable={true}    
+      style={{ bottom: bottomHeight + 60 + 'px', height: `calc(100% - ${bottomHeight + 120}"px")`} }    
+      placeholder="Write your note here..."
+      onInput={() => setContent(editorRef.current.innerHTML)}
+    ></div>
 
-      <div 
-        tabIndex={0} 
-        className=" bg-white border-t fixed top-[60px] right-0 left-0 py-2 no-scrollbar overflow-auto z-10 h-[48px] border-b-[1px]"
-      >
-        <div className='flex gap-2 px-3 '>
-        {/* <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageUpload}
-            className="hidden"
-            id="upload-image"
-          />
-          <label htmlFor="upload-image" className="px-2 py-1 border rounded bg-gray-200 cursor-pointer">
-            <FaImage size={20} />
-          </label> */}
-          <button 
-            onClick={() => formatText('formatBlock', 'H1')} 
-            className={`px-2 py-1 border rounded ${activeFormats.heading ? 'bg-gray-300' : 'bg-gray-200'}`}
-          >
-            <BiHeading size={20} />
-          </button>
-          <button 
-            onClick={() => formatText('insertUnorderedList')} 
-            className={`px-2 py-1 border rounded ${activeFormats.unorderedList ? 'bg-gray-300' : 'bg-gray-200'}`}
-          >
-            <AiOutlineUnorderedList size={20} />
-          </button>
-          <button 
-            onClick={() => formatText('insertOrderedList')} 
-            className={`px-2 py-1 border rounded ${activeFormats.orderedList ? 'bg-gray-300' : 'bg-gray-200'}`}
-          >
-            <AiOutlineOrderedList size={20} />
-          </button>
-          <button 
-            onClick={() => formatText('italic')} 
-            className={`px-2 py-1 border rounded ${activeFormats.italic ? 'bg-gray-300' : 'bg-gray-200'}`}
-          >
-            <AiOutlineItalic size={20} />
-          </button>
-          <button 
-            onClick={() => formatText('bold')} 
-            className={`px-2 py-1 border rounded ${activeFormats.bold ? 'bg-gray-300' : 'bg-gray-200'}`}
-          >
-            <AiOutlineBold size={20} />
-          </button>
-          <button 
-            onClick={() => formatText('foreColor', 'black')} 
-            className={`px-2 py-1 border rounded ${activeFormats.blackText ? 'bg-gray-500' : 'bg-gray-200'}`}
-          >
-            <MdFormatColorText size={20} color="black" />
-          </button>
-          <button 
-            onClick={() => formatText('foreColor', 'red')} 
-            className={`px-2 py-1 border rounded ${activeFormats.redText ? 'bg-red-300' : 'bg-gray-200'}`}
-          >
-            <MdFormatColorText size={20} color="red" />
-          </button>
-          <button 
-            onClick={() => formatText('foreColor', 'blue')} 
-            className={`px-2 py-1 border rounded ${activeFormats.blueText ? 'bg-blue-300' : 'bg-gray-200'}`}
-          >
-            <MdFormatColorText size={20} color="blue" />
-          </button>
-          <button 
-            onClick={() => formatText('foreColor', 'green')} 
-            className={`px-2 py-1 border rounded ${activeFormats.greenText ? 'bg-green-300' : 'bg-gray-200'}`}
-          >
-            <MdFormatColorText size={20} color="green" />
-          </button>
-        </div>
+    <div 
+      className={` bg-gray-50 fixed  right-0 left-0 px-4   z-10 `} style={{bottom: bottomHeight+"px"}}
+    >
+      <div ref={allBtnRef} className='relative flex no-scrollbar overflow-auto  items-center h-[60px] gap-2 pr-[70px] transition-all'>
+        <button onClick={()=>{handleRightScroll()}} className='text-[30px] h-[56px] my-[2px] px-5 fixed right-0 bg-white rounded-l-full shadow-xl font-semibold self-center z-50 active:text-blue-600 transition-colors '><MdKeyboardArrowRight /></button>
+        <button 
+          onClick={() => formatText('formatBlock', 'H1')} 
+          className={`px-2 py-1 border rounded ${activeFormats.heading ? 'text-white bg-gray-600' : ''}`}
+        >
+          <BiHeading size={20} />
+        </button>
+        <button 
+          onClick={() => formatText('insertUnorderedList')} 
+          className={`px-2 py-1 border rounded ${activeFormats.unorderedList ? 'text-white bg-gray-600' : ''}`}
+        >
+          <AiOutlineUnorderedList size={20} />
+        </button>
+        <button 
+          onClick={() => formatText('insertOrderedList')} 
+          className={`px-2 py-1 border rounded ${activeFormats.orderedList ? 'text-white bg-gray-600' : ''}`}
+        >
+          <AiOutlineOrderedList size={20} />
+        </button>
+        <button 
+          onClick={() => formatText('italic')} 
+          className={`px-2 py-1 border rounded ${activeFormats.italic ? 'text-white bg-gray-600' : ''}`}
+        >
+          <AiOutlineItalic size={20} />
+        </button>
+        <button 
+          onClick={() => formatText('bold')} 
+          className={`px-2 py-1 border rounded ${activeFormats.bold ? 'text-white bg-gray-600' : ''}`}
+        >
+          <AiOutlineBold size={20} />
+        </button>
+        <button 
+          onClick={() => formatText('foreColor', 'black')} 
+          className={`px-2 py-1 border rounded ${activeFormats.blackText ? 'text-white bg-gray-900' : ' text-black'}`}
+        >
+          <MdFormatColorText size={20} />
+        </button>
+        <button 
+          onClick={() => formatText('foreColor', 'red')} 
+          className={`px-2 py-1 border rounded ${activeFormats.redText ? 'text-white bg-red-600 ' : ' text-red-500'}`}
+        >
+          <MdFormatColorText size={20}  />
+        </button>
+        <button 
+          onClick={() => formatText('foreColor', 'blue')} 
+          className={`px-2 py-1 border rounded ${activeFormats.blueText ? 'text-white bg-blue-700' : ' text-blue-700 '}`}
+        >
+          <MdFormatColorText size={20}  />
+        </button>
+        <button 
+          onClick={() => formatText('foreColor', 'green')} 
+          className={`px-2 py-1 border rounded ${activeFormats.greenText ? 'text-white bg-green-700' : 'text-green-700'}`}
+        >
+          <MdFormatColorText size={20}  />
+        </button>
       </div>
-      <div onClick={()=>{setIsSendNotes(prev => !prev)}} className='fixed flex justify-center align-middle bottom-10 right-8 rounded-full bg-green-700 text-white p-3'><IoSend className='self-center'/></div>
-
-    {isSendNotes &&  <SendNotesComp NotesSample={[]} noteId={noteId} onClose={()=>{setIsSendNotes(false)}} /> }
     </div>
+
+  {isSendNotes &&  <SendNotesComp NotesSample={[]} noteId={noteId} onClose={()=>{setIsSendNotes(false)}} /> }
+  </div>
   );
 };
 
